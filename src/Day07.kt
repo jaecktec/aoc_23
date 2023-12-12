1d6a617f40645a29de29d9598b605936d73f82e7
Constantin <jaecktec@users.noreply.github.com>
2023-12-12 20:33:09 +0100

    day7 cleanup

--- src/Day07.kt
+++ src/Day07.kt
@@ -10,7 +10,7 @@ enum class Day07Type(val points: Int) {
 
 fun main() {
   val day = 7
-  
+
   fun determineType(hand: String): Day07Type {
     assert(hand.length == 5) { "hand must be 5 characters" }
     val charCountMap = hand.groupingBy { it }.eachCount()
@@ -62,16 +62,15 @@ fun main() {
 
   fun part2(input: List<String>): Long {
     // J is weakest - wer lesen kann ist klar im Vorteil..
-    val strength = listOf( 'J','2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A')
+    val strength = listOf('J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A')
     fun determineTypeWithJoker(hand: String): Day07Type {
       if (!hand.contains("J")) return determineType(hand)
       var highestType = determineType(hand)
       for (i in 0 until 5) {
         if (hand[i] != 'J') continue
-        for (replacement in strength) {
-          if (replacement == 'J') continue
+        for (replacement in strength.drop(1)) {
           val newHand = hand.replaceAt(i, replacement)
-          
+
           val possibleType = if (newHand.contains('J')) {
             determineTypeWithJoker(newHand)
           } else {
@@ -83,7 +82,6 @@ fun main() {
           }
           if (highestType.points == Day07Type.FiveOfAKind.points) break; // doesn't get higher
         }
-        
       }
       return highestType
     }
@@ -111,18 +109,14 @@ fun main() {
         }
       }
 
-    grouped.joinToString("\n").println()
-
     return grouped.foldIndexed(0) { index, acc, triple ->
       acc + (index + 1) * triple.second
     }
   }
 
-//  part1(readInput(day, "test")).validate(6440)
-//  part1(readInput(day, "input")).println()
+  part1(readInput(day, "test")).validate(6440)
+  part1(readInput(day, "input")).println()
 
   part2(readInput(day, "test")).validate(5905)
-
-  // 253659645 too high
   part2(readInput(day, "input")).println()
 }
